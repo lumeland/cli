897486e39200f9799354e919253ec437eff50610
Oscar Otero <oom@oscarotero.com>
2023-01-06 17:16:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Lume CLI
+
+Simple script to run Lume easily, without need to type
+`deno task lume [whatever]`.
+
+## Install
+
+```
+deno install --allow-run --name lume --force --reload https://deno.land/x/lume_cli/mod.ts
+```
+
+## Usage
+
+- `lume init` will run the command
+  `deno run -Ar https://deno.land/x/lume/init.ts` to initialize Lume in the
+  current directory.
+- `lume upgrade-cli` will upgrade this script to the latest version.
+- Any other command is delegated to `deno task lume [...args]`. For example,
+  `lume -s` will run `deno task lume -s`.
---
+++ mod.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env deno
+
+const args = Array.from(Deno.args);
+console.log(args);
+if (args[0] === "init") {
+  args.shift();
+  await run("deno", "run", "-Ar", "https://deno.land/x/lume/init.ts", ...args);
+} else if (args[0] === "upgrade-cli") {
+  await run(
+    "deno",
+    "install",
+    "--allow-run",
+    "--name",
+    "lume",
+    "--reload",
+    "--force",
+    "https://deno.land/x/lume_cli/mod.ts",
+  );
+} else {
+  await run("deno", "task", "lume", ...args);
+}
+
+async function run(...cmd: string[]) {
+  const process = Deno.run({ cmd });
+  return await process.status();
+}
